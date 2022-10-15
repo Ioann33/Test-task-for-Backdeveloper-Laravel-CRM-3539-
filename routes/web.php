534d5af3f03cfa5c43a5499fc5286bdc86aea4c8
ioann <ivan.karmalyk2017@gmail.com>
2022-10-15 23:36:29 +0300

    added routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Category;
+use App\Http\Controllers\Lot;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(route('lots.index'));
 });
+
+Route::resource('/lots', Lot::class);
+Route::resource('/categories', Category::class);
